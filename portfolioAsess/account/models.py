58e1c9026aa0ad2c04c571757d0a7cf8d94ea835
Owzaa <nyondo.olwethu@gmail.comm>
2022-05-01 11:48:03 +0200

    commit on master

--- portfolioAsess/account/models.py
+++ portfolioAsess/account/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # User Account Model:
-class UserDetails(models.Model):
+class User(models.Model):
+        username = models.OneToOneField(User, on_delete=models.CASCADE)
+        homeAddress = models.TextField()
+        phoneNumber = models.CharField(max_length=10)
+        location = models.CharField()
+        
+def __str__(self):
+    return self.name
+    (args)
         
