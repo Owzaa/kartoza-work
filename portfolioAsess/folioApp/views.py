a9fd544bb4b37cecf5f86db2962624aac32b5517
Owzaa <nyondo.olwethu@gmail.comm>
2022-05-02 14:38:53 +0200

    commit on master

--- portfolioAsess/folioApp/views.py
+++ portfolioAsess/folioApp/views.py
@@ -10,30 +10,30 @@ from account.models import UserProfile
 
 # HomeView  function
 def HomeView(request):
-    Template = "portfolioAsess/templates/home/index.html"
-    return render(request,template_name=Template,context_type={}) 
+    Template = "folioApp/templates/home/index.html"
+    return render(request,template_name=Template,context={}) 
 # Sign-up View Function
 def SignUpView(request):
-    Template = "portfolioAsess/templates/sign-up/sign-up.html"
+    Template = "folioApp/templates/sign-up/sign-up.html"
     return render(request,template_name=Template,context={}) 
 # LoginView Function
 def LoginView(request):
-    Template = "portfolioAsess/templates/login/login.html"
+    Template = "folioApp/templates/login/login.html"
     return render(request,template_name=Template,context={}) 
 
 # MapViewDetails Page
 def MapView(request):
-    Template = "portfolioAsess/templates/MapDetail/userMapDetails.html"
+    Template = "folioApp/templates/MapDetail/userMapDetails.html"
     return render(request,template_name=Template,context={}) 
 
 # UserDetailView Function
 def User_DetailsView(request):
-    Template = "portfolioAsess/templates/accopunt/UserDetails/userDetails.html"
+    Template = "folioApp/templates/accopunt/UserDetails/userDetails.html"
     return render(request,template_name=Template,context={}) 
 
 # userEditDetailView Function
 def User_EditDetailsView(request):
-    Template = "portfolioAsess/templates/accopunt/UserEditDetails/userEditDetails.html"
+    Template = "folioApp/templates/accopunt/UserEditDetails/userEditDetails.html"
     return render(request,template_name=Template,context={})
 
 # Listing Our Accounts stored in the Database:
@@ -44,7 +44,7 @@ def User_EditDetailsView(request):
 
 # Listing all Accounts in our TemplateView    
 def ListAccounts(request):
-    user = get_object_or_404(ListAccounts)
-    return render(request,template_name="portfolioAsess/templates/account/List-Accounts.html",
+    user = get_object_or_404(UserProfile)
+    return render(request,template_name="folioApp/templates/account/List-Accounts.html",
             content_type={user})
     
