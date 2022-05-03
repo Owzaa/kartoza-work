f899f8da4e37d87cb8f302b98b72f15431397dbd
Owzaa <nyondo.olwethu@gmail.comm>
2022-05-03 15:34:09 +0200

    commiton master

--- portfolioAsess/account/models.py
+++ portfolioAsess/account/models.py
@@ -14,14 +14,14 @@ class UserProfile(models.Model):
 
 # @receiver Send/Receive Signals for our object 
 # Instances when save button is clicked               
-  
+
 @receiver (post_save,sender=User)
 def create_user_profile(sender,instance,created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
-#@receiver saving our userProfile instance object     
 
+#@receiver saving our userProfile instance object     
 @receiver (post_save,sender=User)
 def save_user_profile(sender, instance, **kwargs):
         instance.save()
@@ -29,4 +29,3 @@ def save_user_profile(sender, instance, **kwargs):
 # return userProfile name as string    
 def __str__(self):
     return self.username
-   
