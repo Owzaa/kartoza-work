b78f89eca7463a5667b5d82b13cefc68f6fd99cd
Owzaa <nyondo.olwethu@gmail.comm>
2022-05-06 17:51:43 +0200

    commit on master

--- portfolioAsess/folioApp/views.py
+++ portfolioAsess/folioApp/views.py
@@ -1,12 +1,37 @@
-from ast import arg
+"""
+Author: Olwethu Theo Nyondo
+Project: Portfolio Application (Views.py)
+Date: 06/05/2022
+Published: https://github.com/Owzaa/repositry/
+
+Book Refferenced(ISBN): Django Documentation
+Release 3.2.12.dev 
+ 
+"""
+
+from collections import UserList
 from msilib.schema import ListView
 from types import MemberDescriptorType
-from django.http import HttpResponse
+from django.http import HttpResponse, QueryDict
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required,permission_required
 from account.models import UserProfile
-from account.forms import ProfileForm
-from account.forms import userProfileForm
+
   
+  
+"""
+                         LIBRARY IMPORTS(DECORATORS)
+                         
+Imported 2 Decorators from our Library contrib.auth.decorators as per Requirement
+from the ASSIGNMENT DOCUMENTATION
+----------------------------------------------------------------------------------
+                         AUTHORISATION DECORATORS:
+
+@permission_required: This will check if user is permitted to ViewPage()
+@login_required: This Checks if user logged in  to view or acess the Page()
+
+"""
+
 
 # HomeView  function
 def HomeView(request):
@@ -18,36 +43,69 @@ def SignUpView(request):
 def LoginView(request):
     return render(request,"login/login.html",{}) 
 
-# MapViewDetails Page
+""" 
+
+                    MAP_PAGE()
+           
+@login_required decorator redirect user to login_url page if not Redirects
+ to login page
+ 
+@user.permission.View: only Auth_Admin can Assess this Page
+"""
+
+@login_required(login_url="/LoginView/")
 def MapView(request):
-    
     return render(request,"MapDetail/MapDetails.html",context={}) 
 
 # UserDetailView Function
+@login_required(login_url="/LoginView/")
 def ProfileDetailsView(request):
     return render(request,"account/UserDetails/userDetails.html",context={}) 
 
 # userEditDetailView Function
+@login_required(login_url="/LoginView/")
 def ProfileEditDetailsView(request):
-    return render(request,"UserEditDetails/userEditDetails.html",context={})
+    return render(request,"account/UserEditDetails/userEditDetails.html",context={})
 
-# Listing Our Accounts stored in the Database:
-# AccountDetailsListingView ==> Subclassed->(ListView) 
-# class ListAccountDetails(ListView):
-#     model = UserProfile
-# Listing all Accounts in our Database to our HTML TemplateView    
+    
+    
+
+@login_required(login_url= 'LoginView')
+@permission_required(perm='folioApp.view_choice', login_url='LoginView')
 def ListAccounts(request):
-     return   render(request,'account/List-Accounts.html',{})
+    return render(request,'account/List-Accounts.html',{})
+ 
+
+
+
+
+
+
+
+ 
     
 """ 
-This View logs in a Mmeber of the SIte
-LEGEND: 
-m => Member
+LOGIN / LOGOUT FUNCTIONS  
 
+loginView - Log in User to our site
+logout - log off User out of our site
+-------------------------------------------------
+LEGENDS: 
 
-Login / logout Views()  Session=> activity 
-by showing who and when on the admin page
+Assigned  m => 'Member' or USER object
+--------------------------------------------------
+LOGIN() FUNCTION
+
+GET: username & password PARAMETERS_OBJECTS Then 
+        use Request(param)
+POST: Verify our User_logging details 
+--------------------------------------------------
+USER ACTIVITY 
+
+request.Session : Activity Log
+--------------------------------------------------
 """
+
 def login(request):
     m = MemberDescriptorType.objects.get(username=request.POST['username'])
     
@@ -61,7 +119,7 @@ def login(request):
 
 def logout(request):
     try:
-        del request.session['member_id']
+        del request.session['user_id']
     except KeyError:
         pass
-    return HttpResponse("You're logged out")
+    return render(request,'login/login.html')
