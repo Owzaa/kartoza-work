6b8f32d7415fd784a93d205e3ae80576171748d2
Owzaa <nyondo.olwethu@gmail.comm>
2022-05-09 16:09:50 +0200

    commit on master

--- portfolioAsess/account/models.py
+++ portfolioAsess/account/models.py
@@ -3,7 +3,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django.urls import reverse_lazy
+''' from django.contrib.gis.gdal import models
+ '''
 
 # UserProfile Model:
 class UserProfile(models.Model):
@@ -12,9 +13,8 @@ class UserProfile(models.Model):
         username = models.OneToOneField(User, on_delete=models.CASCADE)      
         homeAddress = models.TextField()
         phoneNumber = models.CharField(max_length=12)
-        location = models.CharField(max_length=200) 
-# return userProfile name as string    
-    
+        location = models.CharField(max_length=180) 
+# return userProfile name as string     
 def __str__(self):
         return self.username  
 
