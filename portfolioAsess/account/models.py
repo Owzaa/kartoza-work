51a2a2d242bcaf46366a3192c26db2bceaa355e2
Owzaa <nyondo.olwethu@gmail.comm>
2022-05-09 15:12:36 +0200

    commit on master

--- portfolioAsess/account/models.py
+++ portfolioAsess/account/models.py
@@ -7,28 +7,28 @@ from django.urls import reverse_lazy
 
 # UserProfile Model:
 class UserProfile(models.Model):
-        username = models.OneToOneField(User, on_delete=models.CASCADE)
-         
+        firtsName = models.CharField(max_length=100,default=False)
+        lastName = models.CharField(max_length=100,default=False)
+        username = models.OneToOneField(User, on_delete=models.CASCADE)      
         homeAddress = models.TextField()
         phoneNumber = models.CharField(max_length=12)
         location = models.CharField(max_length=200) 
 # return userProfile name as string    
-      
+    
+def __str__(self):
+        return self.username  
 
 # @receiver Send/Receive Signals for our object 
 # Instances when save button is clicked               
 
 @receiver (post_save,sender=User)
-def create_user(created, **kwargs):
+def create_user(created, *args,**kwargs):
     if created:
        create_user_profile(instance=User)
 
 
 #@receiver saving our userProfile instance object     
 @receiver (post_save,sender=User)
-def create_user_profile(instance,self, **kwargs):
-        instance.self.save() 
+def create_user_profile(instance,**kwargs):
+        instance.save() 
 
-
-def __str__(self):
-        return self.username
--- portfolioAsess/account/views.py
+++ portfolioAsess/account/views.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 
 
-# filtering our user Database to list_view
+#   filtering our user Database to list_view
 def user_list(request):
     class Meta:
         model = UserProfile
     users = UserProfile.objects.filter(is_active=True)
     return render (request,'account/List-Accounts.html',{'users':users})
 
-#User Authentication check when a user is requeasting user_details
+#   User Authentication check when a user is requeasting user_details
 def user_detail(request):
     user= get_object_or_404 (is_active=True)
     return render(request,'account/UserDetails/userDetails.html',{})
